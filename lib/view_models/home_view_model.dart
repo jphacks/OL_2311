965b444a35baade5109e2ffa66a30e5e94795a5a
yuuumiravy <misharv.y@gmail.com>
2023-10-28 14:38:09 +0900

    feat: HomeViewModelを作成

--- lib/view_models/home_view_model.dart
+++ lib/view_models/home_view_model.dart
@@ -2,15 +2,15 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kanpai/models/user_model.dart';
 import 'package:kanpai/repositories/user_repository.dart';
 
-class UserListViewModel extends StateNotifier<AsyncValue<List<User>>> {
+class HomeViewModel extends StateNotifier<AsyncValue<List<User>>> {
   final UserRepository _userRepository;
 
-  UserListViewModel(this._userRepository) : super(const AsyncValue.loading()) {
+  HomeViewModel(this._userRepository) : super(const AsyncValue.loading()) {
     _fetchUsers();
   }
 
   void _fetchUsers() {
-    final usersStream = _userRepository.getUsers();
+    final usersStream = _userRepository.getUsersStream();
     usersStream.listen((users) {
       state = AsyncValue.data(users);
     });
