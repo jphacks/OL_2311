aa7c23e3f935c9ffda0cf78184a5f04cd95264ca
kurikin <riku.code0309@gmail.com>
2023-11-16 22:01:59 +0900

    Add logic for color send

--- lib/view/onboarding/question2_screen/tech_area.dart
+++ lib/view/onboarding/question2_screen/tech_area.dart
@@ -1,22 +1,37 @@
-enum TechArea {
-  frontend,
-  backend,
-  infra,
-  hardware,
-  designer;
+import 'package:flutter/material.dart';
 
-  String get label {
-    switch (this) {
-      case TechArea.frontend:
-        return "フロントエンド";
-      case TechArea.backend:
-        return "バックエンド";
-      case TechArea.infra:
-        return "インフラ";
-      case TechArea.hardware:
-        return "ハードウェア";
-      case TechArea.designer:
-        return "デザイナー";
+extension ColorExt on Color {
+  String toCustomString() {
+    return 'color($red,$green,$blue)';
+  }
+}
+
+enum TechArea {
+  frontend('フロントエンド', Color(0xfff03e3e)),
+  backend('バックエンド', Color(0xfff76707)),
+  infra('インフラ', Color(0xffae3ec9)),
+  hardware('ハードウェア', Color(0xff7048e8)),
+  designer('デザイナー', Color(0xff1c7ed6)),
+  other('その他', Color(0xff37b24d));
+
+  const TechArea(this.displayName, this.color);
+
+  final String displayName;
+  final Color color;
+
+  factory TechArea.fromName(String name) {
+    switch (name) {
+      case 'フロントエンド':
+        return TechArea.frontend;
+      case 'バックエンド':
+        return TechArea.backend;
+      case 'インフラ':
+        return TechArea.infra;
+      case 'ハードウェア':
+        return TechArea.hardware;
+      case 'デザイナー':
+        return TechArea.designer;
     }
+    return TechArea.other;
   }
 }
