602ed19b2920c019919a279b687668ee8b30c6dc
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-11-17 19:08:54 +0900

    feat: new kanapi screen ui

--- lib/components/user_icon_panel.dart
+++ lib/components/user_icon_panel.dart
@@ -1,7 +1,10 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kanpai/components/profile_card/icon_chip.dart';
+import 'package:kanpai/components/tag.dart';
 import 'package:kanpai/models/user_model.dart';
+import 'package:kanpai/util/get_user_color.dart';
 
 class UserIconPanel extends HookConsumerWidget {
   const UserIconPanel({
@@ -17,95 +20,100 @@ class UserIconPanel extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final isUnlocked = 0 < count;
 
-    return Container(
-      width: double.infinity,
-      clipBehavior: Clip.antiAlias,
-      decoration: ShapeDecoration(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(7.20),
+    final techAreaStruct = TechAreaStruct.fromText(user.techArea);
+
+    return DottedBorder(
+      padding: const EdgeInsets.all(1),
+      borderType: BorderType.RRect,
+      color: isUnlocked ? Colors.transparent : const Color(0xffBDBDBD),
+      radius: const Radius.circular(20),
+      child: Container(
+        width: double.infinity,
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+          color: isUnlocked ? null : Colors.white,
+          boxShadow: isUnlocked
+              ? [
+                  const BoxShadow(
+                    color: Color(0x3F999999),
+                    blurRadius: 6,
+                    offset: Offset(0, 2),
+                    spreadRadius: 0,
+                  )
+                ]
+              : [],
         ),
-        gradient: const LinearGradient(
-          begin: Alignment(0, -1),
-          end: Alignment(0, 1),
-          colors: [Colors.white, Color(0xFFEAEAEA)],
-          stops: [
-            0.0,
-            1.0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: isUnlocked
+                    ? DecorationImage(
+                        image: AssetImage(techAreaStruct.panelBgImage),
+                        fit: BoxFit.fitWidth,
+                      )
+                    : null,
+              ),
+              child: SizedBox(
+                  height: 128,
+                  child: isUnlocked
+                      ? IconChip(
+                          imageUrl: user.profileImageUrl!,
+                          ringImageUrl: techAreaStruct.ringImage,
+                          size: IconChipSize.lg)
+                      : Center(
+                          child: Image.asset(
+                            techAreaStruct.lockImage,
+                            width: 88,
+                            height: 88,
+                          ),
+                        )),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                gradient: isUnlocked
+                    ? LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          techAreaStruct.panelUnderTextBgGradientBegin,
+                          techAreaStruct.panelUnderTextBgGradientEnd,
+                        ],
+                        stops: const [
+                          0.0,
+                          1.0,
+                        ],
+                      )
+                    : null,
+                color: isUnlocked ? null : const Color(0xffEEEEEE),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    isUnlocked ? user.name! : "? ? ?",
+                    style: const TextStyle(
+                        fontFamily: "Kollektif_sub",
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87),
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Tag(
+                      label: user.techArea!,
+                      color: isUnlocked
+                          ? techAreaStruct.solidBgColor
+                          : const Color(0xFFBDBDBD))
+                ],
+              ),
+            )
           ],
         ),
-        shadows: const [
-          BoxShadow(
-            color: Color(0x3F999999),
-            blurRadius: 6,
-            offset: Offset(0, 2),
-            spreadRadius: 0,
-          )
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(24),
-            child: isUnlocked
-                ? CircleAvatar(
-                    radius: 40,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(999),
-                      child: Image.network(user.profileImageUrl!),
-                    ),
-                  )
-                : Image.asset(
-                    "assets/images/unlock.png",
-                    width: 80,
-                    height: 80,
-                  ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(16),
-            color: const Color(0xffE8E8E8),
-            child: Column(
-              children: [
-                Text(
-                  user.name!,
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                const SizedBox(
-                  height: 6,
-                ),
-                Container(
-                  decoration: const BoxDecoration(
-                      color: Color(0xff2A2A2A),
-                      borderRadius: BorderRadius.all(Radius.circular(999))),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 3, horizontal: 8),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(
-                        "assets/svgs/cup.svg",
-                        height: 12,
-                        theme: const SvgTheme(currentColor: Colors.white),
-                      ),
-                      const SizedBox(
-                        width: 2,
-                      ),
-                      Text(
-                        "$count",
-                        style: const TextStyle(
-                            fontSize: 10,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
       ),
     );
   }
